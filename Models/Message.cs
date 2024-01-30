18686a30e92fc733b474e2868f3176a043b06cb4
Mithra21827 <mithrak21s@gmail.com>
2024-01-30 19:53:56 +0530

    message broker

---
+++ Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MESSGEBROKER.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? TopicMessage { get; set; }
+
+        public int SubsrciptionId { get;set; }
+
+        [Required]
+        public DateTime ExpiresAfter { get; set; }= DateTime.Now.AddDays(1);
+
+        [Required]
+        public string MessageStatus { get; set; } = "NEW";
+
+    }
+}
---
+++ Models/Topic.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MESSGEBROKER.Models
+{
+    public class Topic
+    {
+        [Key]
+        public int Id { set; get; }
+
+        [Required]
+        public string? Name { set; get; }
+    }
+}
